1bebf3067ab1cb38b6718059834f42583cea5843
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-09-30 19:14:03 -0300

    Reinclusão de componente de links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button'; 
 
+import { SocialLinksComponent } from './social-links/social-links.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialLinksComponent
   ],
   imports: [
     BrowserModule,
